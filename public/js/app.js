d45f3fa11242ea90ecd8e11f3f885b8d3bc5e136
marekft <48406719+marekft@users.noreply.github.com>
2019-06-14 20:34:44 +0200

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,3 @@
-console.log('clientside javaScript file is loaded!')
- 
-// fetch('http://localhost:3000/weather?adress=boston').then((response) => {
-//     response.json().then((data) => {
-//         if (data.error) {
-//             console.log(data.error)
-//         } else {
-//             console.log(data.forecast)
-//             console.log(data.location)
-//         }
-//     })
-// })
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 let messageOne = document.querySelector('#message-1')
